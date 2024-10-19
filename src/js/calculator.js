b2a7d3ffb6c2f2d6b4c85fdf9fe95d1e264c9953
MahlaFaridani2006 <mahlafaridani@gmail.com>
2024-10-18 22:30:42 -0700

    'main-calculator-1'

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,16 +1,63 @@
+"use strict";
+/* ui data*/
 const LCD = {
-    history: $('#calculatorHistory'),
-    currentNumber: $('#calculatorCurrentShowNumber'),
-    action: $('#calculatorCurrentShowAction'),
+    history: $('#history'),
+    currentNumber: $('#show-number'),
+    action: $('#show-action'),
+}
+/* process data*/
+const process = {
+    action: '',
+    nextNum: 0,
+    result: 0,
 }
 
-function ClearAll(makeHR = false) {
-    LCD.action.text('')
-    LCD.currentNumber.val(0)
-    if (makeHR) {
-        LCD.history.text('')
-    } else if (makeHR === false) {
-        LCD.history.append('<hr/>')
+/*
+clear ui & Process
+params isAll: boolean
+*/
+function doClear(isAll = false) {
+    LCD.action.text('');
+    LCD.currentNumber.val('');
+    process.action = '';
+
+    if (isAll) {
+        LCD.history.text('');
+    } else {
+        process.nextNum = 0;
+        process.result = 0;
+        LCD.history.append('<hr/>');
     }
 
+}
+
+/*
+get action from user
+* params action must be one of '* - / +'
+*/
+function setAction(action) {
+    LCD.action.text(action);
+    if (process.action !== '') {
+        doAction(action);
+    } else {
+        process.action = action;
+    }
+}
+/*
+get action from setAction
+params action:number
+*/
+function doAction(action) {
+    const result = 0;
+    addHistory(result);
+    process.action = action;
+}
+
+/*
+just for showing history in ui
+params result:number
+*/
+function addHistory(result) {
+    let elements=(`${process.result} ${process.action} ${process.nextNum}=${result}`);
+    LCD.history.append(elements);
 }
